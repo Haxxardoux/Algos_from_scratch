d21dd3b1013a6b8bc43c13b80e0072648113a013
Haxxardoux <turbowilliam@gmail.com>
2020-02-18 15:43:04 -0500

    general case neural network working

--- algos_from_scratch.py
+++ algos_from_scratch.py
@@ -62,20 +62,20 @@ weights = loadmat('C:/Users/turbo/Python projects/Algos from scratch/Algos_from_
 test = loadmat('C:/Users/turbo/Python projects/Algos from scratch/Algos_from_scratch/ex3data1.mat')
 input_array = test['X']
 output_array = test['y']
-
+input_array = np.array(input_array)
 # Weights for first layer
 weights_1 = weights['Theta1']
 
 # Weights for second layer
 weights_2 = weights['Theta2']
 
-def sigmoid(x, theta_vector):
+def sigmoid(x):
     """
     Computes estimated y values given theta parameters and x values. 
     Returns: List with same number of elements as x matrix, each element is estimated y value
     """
-    foo = np.matmul(x, theta_vector)
-    return 1/(1+np.exp(-foo))
+    #foo = np.matmul(x, theta_vector)
+    return 1/(1+np.exp(-x))
 
 def gradient(x, theta_vector):
     """
@@ -97,43 +97,84 @@ def initialize_random_weights(layer_n_list):
             for j in np.arange(layer_n_list[n]):
                 x_feature_list.append(random.uniform(-k, k))
             weights_array.append(x_feature_list)
-        weights_matrix.append(weights_array)
+        weights_matrix.append(np.array(weights_array))
+
+    for i in np.arange(len(layer_n_list)-2):
+        weights_matrix[i+1] = np.concatenate((np.array([[1]]*weights_matrix[i+1].shape[0]), weights_matrix[i+1]), axis=1)
 
     return weights_matrix
 
 # Temp
-test_y = [i[0] for i in test['y']==10]
-test_x = test['X'][test_y]
+transformed_y = []
+for i in [i[0] for i in test['y']]:
+    foo = [0,0,0,0,0,0,0,0,0,0].copy()
+    foo[i-1]=True
+    transformed_y.append(foo)
+output_array = np.array(transformed_y)
 
-def propagate(observation, weights_matrix, n_minus_1_list):
-    node_matrix = [observation.tolist()]
-    for n in np.arange(len(n_minus_1_list)):
-        node_array = []
-        for i in np.arange(n_minus_1_list[n]):
-            node_array.append(np.dot(weights_matrix[n][i], node_matrix[n]))
-        node_matrix.append(node_array)
-    return node_matrix
+# shuffle the input and output arrays in the same order (seed 4)
+random.Random(4).shuffle(input_array)
+random.Random(4).shuffle(output_array)
 
-def neural_net(input_array, layer_n_list):
+def backpropagate(transformed_y, nodes_matrix, weights_matrix, n_minus_1_list):
+    nodes_matrix = nodes_matrix[::-1]
+    weights_matrix = weights_matrix[::-1]
+    output_error = np.array(nodes_matrix[0])-np.array(transformed_y)
+    error_matrix = [output_error.tolist()]
+    delta = output_error
+    for i, k in enumerate(n_minus_1_list):
+        gz = np.array(nodes_matrix[i+1])*(1-np.array(nodes_matrix[i+1]))
+        delta = np.dot(np.array(weights_matrix[i]).T,delta) * gz
+        error_matrix.append(delta.tolist())
+    #print(np.array(error_matrix[0]))
+    #print(np.array(nodes_matrix[1]))
+    return error_matrix 
+
+def propagate(input_array, weights_matrix, layer_n_list):
+    node_array = [input_array]
+    for i in np.arange(len(layer_n_list)-1):
+        foo = np.matmul(node_array[0], weights_matrix[0].T)
+        foo = sigmoid(foo)
+        foo = np.concatenate((np.array([[1]]*foo.shape[0]), foo), axis=1)
+        node_array.append(foo)
+
+    foo = np.matmul(node_array[-1], weights_matrix[-1].T)
+    foo = sigmoid(foo)
+    node_array.append(foo)
+
+    return node_array
+
+def neural_net(input_array, layer_n_list, gamma, n_iterations):
     # Add column of 1s at the beginning
     input_array = np.concatenate((np.array([[1]]*input_array.shape[0]),input_array), axis=1)
     layer_N_list = [input_array.shape[1]] + layer_n_list
-    
+    temp = input_array
+
+    # Procedurally initiate matrix of weights
     weights_matrix = initialize_random_weights(layer_N_list)
 
-    test = input_array[0]
+    # Gradient descent iteration
+    error = []
+    for i in np.arange(n_iterations):
+        input_array = temp
+        node_array = propagate(input_array,weights_matrix,layer_n_list)
 
-    node_matrix = propagate(test, weights_matrix, layer_n_list)
+        # add backpropogate function
+        d3 = node_array[2] - output_array
+        d2 = np.matmul(d3, weights_matrix[1][:,1:]) * (node_array[1][:,1:]*(1-node_array[1][:,1:]))
+        Delta2 = np.matmul(d3.T, node_array[1])
+        Delta1 = np.matmul(d2.T, node_array[0])
+        weights_matrix[1] -= (Delta2 + gamma*weights_matrix[1]) / (5000)
+        weights_matrix[0] -= (Delta1 + gamma*weights_matrix[0]) / (5000)
+        error.append(d3.sum())
 
-    print(node_matrix[2][0])
+    print((output_array - node_array[2].round()).mean())
+    plt.plot(error)
+    plt.show()
 
-    # propagate(debugging_array[0], weights_array)
-    # for i, n in enumerate(layer_n_list[:-1]):
-    #     print('array', debugging_array.shape)
-    #     print('weights', np.array(weights_matrix[i]).shape)
-    #     debugging_array = propagate(debugging_array[0], weights_matrix[i], n)
-    return debugging_array
+    return 
 
-#print(len(initialize_random_weights([401, 25, 10])[0]))
-neural_net(input_array, [25,10])
+
+print(neural_net(input_array, [25,10], 0.097,300))
+
 
